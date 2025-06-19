22a3ff0bc9655ef5b0ed69553c83ff89761d6ee8
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2025-06-19 10:28:40 +0200

    Update 02_amas_alignment_parallel.sh

--- 00_scripts/02_amas_alignment_parallel.sh
+++ 00_scripts/02_amas_alignment_parallel.sh
@@ -3,9 +3,9 @@
 #SBATCH --job-name=beast_alignment
 ##SBATCH --time=24:00
 #SBATCH --ntasks=1
-#SBATCH -p gdec
-#SBATCH --nodelist=gdecnode02
-#SBATCH --mem=450G
+#SBATCH -p fast
+#SBATCH --nodelist=hpcnode27
+##SBATCH --mem=450G
 #SBATCH -c 32
 #SBATCH --mail-user=pierrelouis.stenger@gmail.com
 #SBATCH --mail-type=ALL 
