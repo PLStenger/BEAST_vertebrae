3668e19ad0af5626718f8dca8d599ff9c2ad1476
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2025-07-02 13:25:33 +0200

    Update 02_amas_alignment_other.sh

--- 00_scripts/02_amas_alignment_other.sh
+++ 00_scripts/02_amas_alignment_other.sh
@@ -5,7 +5,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8     
 #SBATCH -p gdec
-#SBATCH --mem=1000G            
+#SBATCH --mem=500G            
 #SBATCH --mail-user=pierrelouis.stenger@gmail.com
 #SBATCH --mail-type=ALL 
 #SBATCH --error="/home/plstenge/BEAST_vertebrae/BEAST_vertebrae/00_scripts/02_amas_alignment_other.err"
