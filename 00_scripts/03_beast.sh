072d8ac36d98a3a6d11155e0d5d5a2987e68969a
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2025-07-24 10:32:14 +0200

    Create 03_beast.sh

---
+++ 00_scripts/03_beast.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=03_beast
+#SBATCH --ntasks=1
+#SBATCH -p smp
+#SBATCH --mem=1000G
+#SBATCH --mail-user=pierrelouis.stenger@gmail.com
+#SBATCH --mail-type=ALL 
+#SBATCH --error="/home/plstenge/BEAST_vertebrae/BEAST_vertebrae/00_scripts/03_beast.err"
+#SBATCH --output="/home/plstenge/BEAST_vertebrae/BEAST_vertebrae/00_scripts/03_beast.out"
+
+KR_DIR="/home/plstenge/BEAST_vertebrae/BEAST_vertebrae/03_beast"
+
+module load java/oracle-1.8.0_45
+module load BEAST/2.5.2
+
+beast test.xml
