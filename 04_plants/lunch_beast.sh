3919daacb35915920bfada27efb37e636ed6edf8
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2025-08-05 15:17:23 +0200

    Update lunch_beast.sh

--- 04_plants/lunch_beast.sh
+++ 04_plants/lunch_beast.sh
@@ -5,6 +5,13 @@ conda activate beast_latest
 
 cd /scratch_vol0/fungi/BEAST_vertebrae/04_plants
 
+# Masquer les anciennes libs conda BEAGLE
+for f in libhmsbeagle.so libhmsbeagle.so.1 libhmsbeagle-jni.so libhmsbeagle-cpu-sse.so libhmsbeagle-cpu.so libhmsbeagle-cpu-sse.so.31.0.0 libhmsbeagle-cpu.so.31.0.0 libhmsbeagle.so.1.3.2; do
+  if [ -f "$CONDA_PREFIX/lib/$f" ]; then
+    mv "$CONDA_PREFIX/lib/$f" "$CONDA_PREFIX/lib/$f.bak"
+  fi
+done
+
 export LD_LIBRARY_PATH=$HOME/lib:$LD_LIBRARY_PATH
 export JAVA_OPTS="-Djava.library.path=$HOME/lib"
 
