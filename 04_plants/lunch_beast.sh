0d4437e3bd85e9b3d9a59097dbde946b15240777
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2025-08-05 15:15:33 +0200

    Update lunch_beast.sh

--- 04_plants/lunch_beast.sh
+++ 04_plants/lunch_beast.sh
@@ -5,7 +5,7 @@ conda activate beast_latest
 
 cd /scratch_vol0/fungi/BEAST_vertebrae/04_plants
 
-export LD_LIBRARY_PATH=$HOME/lib:$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$HOME/lib:$LD_LIBRARY_PATH
 export JAVA_OPTS="-Djava.library.path=$HOME/lib"
 
 beast -beagle Murat_parameters.xml
