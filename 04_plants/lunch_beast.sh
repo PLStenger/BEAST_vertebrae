d8ea210ce8b5f89c8f47512af311c6b8a3b882c5
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2025-08-05 14:57:20 +0200

    Update lunch_beast.sh

--- 04_plants/lunch_beast.sh
+++ 04_plants/lunch_beast.sh
@@ -5,5 +5,7 @@ conda activate beast
 
 cd /scratch_vol0/fungi/BEAST_vertebrae/04_plants
 
+export JAVA_LIBRARY_PATH=/usr/local/lib
+
 #beast -threads 4 Murat_parameters.xml
 beast -beagle Murat_parameters.xml
