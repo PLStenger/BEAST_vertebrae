e8f19ca8bed7546e124213df97d81f7a24f73838
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2025-08-14 11:21:13 +0200

    Update lunch_beast_hpc2.sh

--- 04_plants/lunch_beast_hpc2.sh
+++ 04_plants/lunch_beast_hpc2.sh
@@ -42,5 +42,4 @@ WORKING_DIRECTORY=/home/plstenge/BEAST_vertebrae/BEAST_vertebrae/04_plants/beast
 
 cd $WORKING_DIRECTORY
 
-./beast /home/plstenge/BEAST_vertebrae/BEAST_vertebrae/04_plants/Murat_parameters_cleaned_calibrated.xml
-#beast /home/plstenge/BEAST_vertebrae/BEAST_vertebrae/04_plants/Murat_parameters_cleaned_calibrated.xml
+./beast /home/plstenge/BEAST_vertebrae/BEAST_vertebrae/04_plants/Murat_parameters_cleaned_calibrated_all_prior.xml
